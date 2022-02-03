c4af99174c49439a8572f991508624e13c708859
srikar chavali <chavalisrikar@gmail.com>
2022-02-03 16:06:47 +0000

    added crud, readall, deletemany functionality

--- src/app.js
+++ src/app.js
@@ -1,21 +1,39 @@
 const yargs = require("yargs");
 const { client, connection} = require('./db/connection');
 
-const {addMovie} = require('./utils');
+const {addMovie, addMany, readOne, readAll, deleteItem, deleteAll, updateOne,} = require('./utils');
 const app = async (yargsObj) =>{
     try {
         const collection = await connection();
 
         if(yargsObj.add){
             // add movie to mongodb database, needs collection and sucess message.
-            await addMovie(collection, {title: yargsObj.title, actor: yargsObj.actor})
-        }else{
+            await addMovie(collection, {title: yargsObj.title, actor: yargsObj.actor, })
+        }else if(yargsObj.addMany){
+            await addMany(collection, yargsObj.movies)
+        }else if (yargsObj.readOne) {
+            await readOne(collection, {
+            title: yargsObj.title,  
+            actor: yargsObj.actor,
+            });
+        }else if (yargsObj.readall) {
+            await readAll(collection);
+        }else if (yargsObj.update) {
+            await updateOne(collection, yargsObj); //Syntax: node src/app.js --update --title/etc="old"  --newtitle/etc="new value"
+        } else if (yargsObj.deleteone) {
+            await deleteItem(collection, yargsObj); 
+        } else if (yargsObj.deleteall) {
+            await deleteAll(collection, yargsObj);
+        } 
+        else{
             console.log("Incorrect command");
         }
-        client.close();
     } catch (error) {
         console.log(error);
+    }finally{
+        client.close();
     }
 }
 
 app(yargs.argv);
+console.log(yargs.argv);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,94 @@
 
-const { Collection } = require("mongodb")
+const { Collection } = require("mongodb");
+const { client } = require("../db/connection");
+
+
 
 exports.addMovie = async (Collection, movieObj) => {
+    console.log(movieObj);
     await Collection.insertOne(movieObj);
 }
 
+exports.addMany = async (Collection, movieObj) => {
+    try {
+        await Collection.insertMany(movieObj);
+    } catch (error) {
+        console.log("wrong");
+    }
+    finally{
+        client.close();
+    }
+    
+}
+
+//Read
+//Read by key value
+exports.readOne = async (collection, yargsObj) => {
+    if (yargsObj.title) {
+      read = yargsObj.title;
+      results = await collection.find({ title: read }).toArray();
+      console.log(`Filtered collection by ${yargsObj.title} and found the following document(s):`, results);
+    } else if (yargsObj.actor) {
+      read = yargsObj.actor;
+      results = await collection.find({ actor: read }).toArray();
+      console.log(`Filtered collection by ${yargsObj.actor} and found the following document(s):`, results);
+    }
+  };
+  //Read All
+  exports.readAll = async (collection) => {
+    results = await collection.find({}).toArray();
+    console.log("Found all documents from the collection =>", results);
+  };
+  
+  //Update - Syntax would be node src/app.js --update --title/etc="old"  --newtitle/etc="new value"
+  exports.updateOne = async (collection, yargsObj) => {
+    if (yargsObj.newtitle) {
+      const updateResult = await collection.updateOne(
+        { title: yargsObj.title },
+        {
+          $set: { title: yargsObj.newtitle },
+        }
+      );
+      console.log("Updated documents =>", updateResult);
+    } else if (yargsObj.newactor) {
+      const updateResult = await collection.updateOne(
+        { actor: yargsObj.actor },
+        {
+          $set: { title: yargsObj.newactor },
+        }
+      );
+      console.log("Updated documents =>", updateResult);
+    }
+  };
+  
+  //Delete One Instance
+  exports.deleteItem = async (collection, yargsObj) => {
+    if (yargsObj.title) {
+      deleteResult = await collection.deleteOne({ title: yargsObj.title });
+      console.log(`${deleteResult.deletedCount} item(s) deleted.`);
+    } else if (yargsObj.actor) {
+      deleteResult = await collection.deleteOne({ actor: yargsObj.actor });
+      console.log(`${deleteResult.deletedCount} item(s) deleted.`);
+    } else if (yargsObj.id) {
+      deleteResult = await collection.deleteOne({ _id: yargsObj.id });
+      console.log(`${deleteResult.deletedCount} item(s) deleted.`);
+    } else {
+      console.log("No files found with that value.");
+    }
+  };
+  
+  //Delete All Instances
+  exports.deleteAll = async (collection, yargsObj) => {
+    if (yargsObj.title) {
+      deleteResult = await collection.deleteMany({ title: yargsObj.title });
+      console.log(`${deleteResult.deletedCount} item(s) deleted.`);
+    } else if (yargsObj.actor) {
+      deleteResult = await collection.deleteMany({ actor: yargsObj.actor });
+      console.log(`${deleteResult.deletedCount} item(s) deleted.`);
+    } else if (yargsObj.id) {
+      deleteResult = await collection.deleteMany({ _id: yargsObj.id });
+      console.log(`${deleteResult.deletedCount} item(s) deleted.`);
+    } else {
+      console.log("No files found with that value.");
+    }
+  };
